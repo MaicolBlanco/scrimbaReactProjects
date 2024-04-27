9a8312b0682e33523b595bbe01fcb6e239112b88
maicol blanco <m@artcompanysystem.com>
2024-04-26 22:15:40 -0500

    Feat: I take a test to learn Typecrip

---
+++ src/Components/ProjectSetup/Navbar.tsx
@@ -0,0 +1,20 @@
+export const Navbar = (): JSX.Element => {
+  return(
+    <nav>
+      <p>React Course - Project 1</p>
+    </nav>
+  );
+}
+
+/* import React from 'react';
+import './Navbar.css'
+
+export default function Navbar() {
+  return(
+    <nav className='container-nav'>
+      <span className='logo-react'>†~ ReactFacts</span>
+      <p className='extra-title'>React Course - Project 1</p>
+    </nav>
+  );
+};
+ */
---
+++ src/Components/Test/index.tsx
@@ -0,0 +1,12 @@
+import { RandomFox } from "./RandomFox";
+
+const Test = ():JSX.Element =>{
+  return(
+    <>
+      <h1>Test</h1>
+      <RandomFox/>
+    </>
+  );
+}
+
+export {Test};
