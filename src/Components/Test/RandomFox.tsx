bdbaddeb8a64e8a7ce24d26a977f176955f95290
maicol blanco <m@artcompanysystem.com>
2024-04-30 10:49:40 -0500

    Feat: Here I use the useRef hook

--- src/Components/Test/RandomFox.tsx
+++ src/Components/Test/RandomFox.tsx
@@ -1,10 +1,20 @@
+//import React from 'react';
+import { useRef } from "react";
 // interface Props {
 //   image: string;
 // }
 type Props = { image: string };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
+
   return (
-    <img width={300} height="auto" src={image} style={{ borderRadius: 15 }} />
+    <img
+      ref={node}
+      width={320}
+      height="auto"
+      src={image}
+      style={{ borderRadius: 15 }}
+    />
   );
 };
--- src/Components/Test/index.tsx
+++ src/Components/Test/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import React from "react";
 import { RandomFox } from "./RandomFox";
 
 // generate a random function between 1 and 123
@@ -10,18 +11,23 @@ const generateId = () => Math.random().toString(36).substr(2, 9);
 type ImagenItem = { id: string; url: string };
 
 const Test = (): JSX.Element => {
-  //const [images, setImages] = useState<{id:string, url:string}[]>([
-  const [images, setImages] = useState<ImagenItem[]>([
-    //const [images, setImages] = useState<Array<ImagenItem>>([
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-  ]);
+  //const [images, setImages] = useState<{id:string, url:string}[]>([]);
+  const [images, setImages] = useState<ImagenItem[]>([]);
+  //const [images, setImages] = useState<Array<ImagenItem>>([]);
+
+  const addNewFox: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    const newImagenItem: ImagenItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`,
+    };
+
+    setImages([...images, newImagenItem]);
+  };
 
   return (
     <main>
       <h1>Test</h1>
+      <button onClick={addNewFox}>Add new fox</button>
       {images.map(({ id, url }) => (
         <div key={id} style={{ padding: 4 }}>
           <RandomFox image={url} />
