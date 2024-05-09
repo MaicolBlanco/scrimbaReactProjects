3cc92621e6073ad2e742cf852b1e5e7b06e54a1c
maicol blanco <m@artcompanysystem.com>
2024-05-09 08:48:30 -0500

    Feat: I continue with the project, learning more concepts and applying Typescript.

--- src/Components/AirBnbClone/Components/Card.tsx
+++ src/Components/AirBnbClone/Components/Card.tsx
@@ -1,8 +1,57 @@
+import "../Styles/Card.css";
 
-const Card = () =>{
-  return(
-    <p>Replace this</p>
-  );
+interface CardProps {
+  img: string;
+  description: string;
+  rating: string | number;
+  reviewCount: number;
+  country: string;
+  title: string;
+  price: number;
+  openSpots: number;
+  location: string;
 }
+
+const Card = (props: CardProps): JSX.Element => {
+  // console.log(props);
+  const {
+    img,
+    description,
+    rating,
+    reviewCount,
+    country,
+    title,
+    price
+  } = props;
+
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  return (
+    <div className="airbnb-card">
+      {badgeText ? <div className="airbnb-card--badge">{badgeText}</div> : null}
+      {/* Otra forma de hacerlo es con operador logico */}
+      {/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
+      <img src={img} alt={description} />
+      <div className="airbnb-card--stats">
+        <img
+          src="https://pngfre.com/wp-content/uploads/star-png-image-pngfre-44.png"
+          alt="star icon."
+        />
+        <span>{rating}</span>
+        <span className="star--card">({reviewCount}) • </span>
+        <span className="star--card">{country}</span>
+      </div>
+      <p>{title}</p>
+      <p>
+        <span className="airbnb-bold">From ${price}</span> / person
+      </p>
+    </div>
+  );
+};
 
 export default Card;
--- src/Components/AirBnbClone/index.tsx
+++ src/Components/AirBnbClone/index.tsx
@@ -1,13 +1,54 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Card from "./Components/Card";
+import data from "./Components/data";
+import "./Styles/Card.css";
 
 const AirBnbClone = () => {
+  // console.log(data);
+  const cards = data.map((item) => {
+    // console.log("rating" + card.stats.rating);
+    return (
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        description={item.description}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+        location={item.location}
+      />
+    );
+  });
+
   return (
     <div style={{ marginTop: 30 }}>
       <Navbar />
       <Hero />
-      <Card />
+      {/* <Card
+        img={"https://clonebnb-experience.vercel.app/images/katie-zaferes.png"}
+        rating={"5.0"}
+        reviewCount={6}
+        country={"USA"}
+        title={"Life Lessons with Katie Zaferes"}
+        price={136}
+      /> */}
+      <section className="airnbn-cards-list">{cards}</section>
+      {/* {data.map((item) => (
+        <Card
+          key={item.id}
+          img={item.coverImg}
+          description={item.description}
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          country={item.location}
+          title={item.title}
+          price={item.price}
+        />
+      ))} */}
     </div>
   );
 };
